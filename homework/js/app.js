a105fc3c71eef577250bb04d3f2ed9b5f7d457a8
ruthsavchenko <72493002+ruthsavchenko@users.noreply.github.com>
2021-06-08 13:38:41 +0300

    Add files via upload

---
+++ homework/js/app.js
@@ -0,0 +1,72 @@
+$(document).ready(() => {
+    const renderCountries = countries => {
+        console.log(countries);
+        let htmlResult = '';
+        for(let country of countries) {
+            htmlResult += `<tr><td>${country.response.name}</td></tr>`;
+        }
+        $('table#countries tbody').html(htmlResult);
+    }
+    const getData = () => {
+        $.ajax({
+            url: 'https://api-football-v1.p.rapidapi.com/v3/countries',
+            headers: {
+                "x-rapidapi-key": "416f158753msh7d5c5353b0536e6p161137jsnbf07d7ea1bef",
+                "x-rapidapi-host": "api-football-v1.p.rapidapi.com",
+                "useQueryString": true
+            }
+        }).then(data => {
+            console.log(data);
+            renderCountries(data.map(country => {
+                return {name: country.response.name};
+            }))
+        })
+    }
+    getData();
+
+    $.ajax({
+        url: 'https://api-football-v1.p.rapidapi.com/v3/leagues',
+        data: {
+            code: 'ES'
+        },
+        headers: {
+            "x-rapidapi-key": "416f158753msh7d5c5353b0536e6p161137jsnbf07d7ea1bef",
+            "x-rapidapi-host": "api-football-v1.p.rapidapi.com",
+            "useQueryString": true
+        }
+    }).then(data => {
+        console.log(data);
+    })
+
+    $.ajax({
+        url: 'https://api-football-v1.p.rapidapi.com/v3/standings',
+        data: {
+            league: 140,
+            season: '2020'
+        },
+        headers: {
+            "x-rapidapi-key": "416f158753msh7d5c5353b0536e6p161137jsnbf07d7ea1bef",
+            "x-rapidapi-host": "api-football-v1.p.rapidapi.com",
+            "useQueryString": true
+        }
+    }).then(data => {
+        console.log(data);
+    })
+
+    $.ajax({
+        url: 'https://api-football-v1.p.rapidapi.com/v3/players',
+        data: {
+            team: 529,
+            season: '2020'
+        },
+        headers: {
+            "x-rapidapi-key": "416f158753msh7d5c5353b0536e6p161137jsnbf07d7ea1bef",
+            "x-rapidapi-host": "api-football-v1.p.rapidapi.com",
+            "useQueryString": true
+        }
+    }).then(data => {
+        console.log(data);
+    })
+
+
+});
